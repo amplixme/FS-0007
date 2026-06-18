a1f1d676469da59e5d4dcddc86830ace51bf3f0e
julio-urquiza <julio.cesar.uquiza4@gmail.com>
2026-06-18 00:35:33 -0300

    fix: correct import statement for errorHandler in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import errorHandler from "./src/middlewares/error.middleware.js"
+import {errorHandler} from "./src/middlewares/error.middleware.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;

90c8ef27b25026098f062e8e2d81b75902b3e42f
julio-urquiza <julio.cesar.uquiza4@gmail.com>
2026-06-22 12:02:33 -0300

    Implement JWT authentication middleware and configure passport strategy

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import {errorHandler} from "./src/middlewares/error.middleware.js"
 import router from "./src/routers/auth.routes.js";
+import passport from "./src/config/passport.jwt.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize())
 app.use(router)
 app.use(errorHandler)
 app.listen(PORT, () => {

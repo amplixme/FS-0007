b1310f6f26d1d01d5900ab10d40fcc8f749dbd09
julio-urquiza <julio.cesar.uquiza4@gmail.com>
2026-06-22 13:34:35 -0300

    Refactor routing structure and add documentation for new routes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
 import express from "express";
 import cors from "cors";
-import {errorHandler} from "./src/middlewares/error.middleware.js"
-import router from "./src/routers/auth.routes.js";
+import router from "./src/routes/index.js";
 import passport from "./src/config/passport.jwt.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 app.use(passport.initialize())
-app.use(router)
-app.use(errorHandler)
+app.use("api/",router)
+
 app.listen(PORT, () => {
   console.log(`Server corriendo en puerto ${PORT}`);
 });

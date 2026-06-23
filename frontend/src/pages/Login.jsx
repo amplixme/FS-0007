8eeb278a24e1ca5bcedf409698a7e325e90cd56c
MartinoMasson <martinomasson2@gmail.com>
2026-06-23 16:21:02 -0300

    Página de registro con validación

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 function Login() {
+  const location = useLocation();
+  const successMessage = location.state?.successMessage || "";
   return (
     <main>
+      {successMessage && <p className="text-green-500">{successMessage}</p>}
       <h1>Iniciar sesión</h1>
     </main>
   );

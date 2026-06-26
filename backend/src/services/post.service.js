bbad512d5ed4d8e779536e16ac20a5e8ebe9f7bc
Santiago Choque <santy.choque@gmail.com>
2026-06-26 16:35:44 -0300

    añadiendo endpoist para listar post con y sin id

--- backend/src/services/post.service.js
+++ backend/src/services/post.service.js
@@ -1,4 +1,5 @@
 import prisma from "../repository/Prisma/prisma.db.js";
+import CustomError from "../utils/customError.js";
 
 export const create = async (title, content, authorId) => {
   const newPost = await prisma.post.create({
@@ -17,4 +18,45 @@ export const create = async (title, content, authorId) => {
   });
 
   return newPost;
+};
+
+export const getAllPublishedPosts = async () => {
+  return await prisma.post.findMany({
+    where: {
+      published: true, 
+    },
+    include: {
+      author: {
+        select: {
+          name: true, 
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc", 
+    },
+  });
+};
+
+export const getPostById = async (id) => {
+ 
+  const postId = parseInt(id, 10);
+  if (isNaN(postId)) throw new CustomError(400, "ID inválido");
+
+  const post = await prisma.post.findUnique({
+    where: { id: postId },
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  if (!post) {
+    throw new CustomError(404, "El post no existe");
+  }
+
+  return post;
 };

e0835112836a8afb24824cc0f6df6002aa2188c7
German Orellano <gorellano.dev@gmail.com>
2026-07-06 11:03:46 -0300

    feat: add filter category

--- backend/src/services/post.service.js
+++ backend/src/services/post.service.js
@@ -21,15 +21,26 @@ export const create = async (title, content, authorId, published) => {
   return newPost;
 };
 
-export const getAllPublishedPosts = async () => {
+export const getAllPublishedPosts = async (category) => {
+  const where = {
+    published: true,
+  };
+
+  if (category) {
+    where.categories = {
+      some: {
+        slug: category,
+      },
+    };
+  }
+
   return await prisma.post.findMany({
-    where: {
-      published: true,
-    },
+    where,
     include: {
       author: {
         select: {
           name: true,
+          categories: true,
         },
       },
     },

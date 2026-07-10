dad7527ff54c1b38697634596bc92db8de69874a
Santiago Choque <santy.choque@gmail.com>
2026-07-10 12:01:48 -0300

    implementando gestion de categorias

--- frontend/src/services/category.service.js
+++ frontend/src/services/category.service.js
@@ -11,3 +11,41 @@ export const getAll = async () => {
         );
     }
 };
+
+export const createCategory = async (name, slug) => {
+    try {
+        const response = await api.post("/categories", { name, slug });
+        return response.data;
+    } catch (error) {
+        throw new Error(
+            error.response?.data?.message || "Error al crear la categoría",
+            { cause: error }
+        );
+    }
+};
+
+
+export const updateCategory = async (id, name, slug) => {
+    try {
+        const response = await api.put(`/categories/${id}`, { name, slug });
+        return response.data;
+    } catch (error) {
+        throw new Error(
+            error.response?.data?.message || "Error al actualizar la categoría",
+            { cause: error }
+        );
+    }
+};
+
+
+export const deleteCategory = async (id) => {
+    try {
+        const response = await api.delete(`/categories/${id}`);
+        return response.data;
+    } catch (error) {
+        throw new Error(
+            error.response?.data?.message || "Error al eliminar la categoría",
+            { cause: error }
+        );
+    }
+};

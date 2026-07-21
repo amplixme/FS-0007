2dad5f1f2583693a5ea4421676053ec947df7904
Martino Masson <Martinomasson2@gmail.com>
2026-07-20 22:12:10 -0300

    arreglo al agregar Posts

--- backend/src/validation/post.schema.js
+++ backend/src/validation/post.schema.js
@@ -4,5 +4,6 @@ export const postSchema = z.object({
   title: z.string({ required_error: "El título es requerido" }).min(1),
   content: z.string({ required_error: "El contenido es requerido" }).min(1),
   published: z.boolean().optional().default(false),
+  categoryIds: z.array(z.string()).optional(),
   coverImage: z.string().optional(),
 });
